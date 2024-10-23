741879ad731897854b2ea3727bb15462b636e273
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2024-10-23 16:49:15 +0900

    Add axios dependency and fetch characters from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async () => {
+    const apiUrl = "https://stand-by-me.herokuapp.com/api/v1/characters";
+
+    const result = await axios.get(apiUrl);
+    console.log(result);
+  };
   return <div className="App">hello world</div>;
 }
 
